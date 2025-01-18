18c83a52b6294a17af792e82faa72d6a41385a09
NT-Nova <teslatnova@gmail.com>
2025-01-18 22:58:40 +0100

    Enhance blockchain data check to include size validation before starting node

--- entrypoint.sh
+++ entrypoint.sh
@@ -191,7 +191,7 @@ main() {
 
         # Determine whether blockchain data (a folder starting with the network name) exists,
         # and that the data folder size is greater than 1 GB.
-        if compgen -G "${ALGORAND_DATA}/${NETWORK}*" > /dev/null; then
+        if compgen -G "${ALGORAND_DATA}/${NETWORK}*" > /dev/null && [ "${DATA_SIZE}" -gt "${ONE_GB}" ]; then
             # Your code when both conditions are true
             log_info "Blockchain data folder detected (matching ${NETWORK}*) and the data folder size is greater than 1 GB.(${DATA_SIZE})"
             start_node
